6a1ea0b91fcb7aa01235e7ae38511e1e0003d6ab
15003480252 <1002117500@qq.com>
2024-12-31 02:16:10 +0800

    ad网站更新

--- web_ad/src/main/java/com/adserver/repository/AdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.adserver.repository;
-
-import com.adserver.model.Ad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdRepository extends JpaRepository<Ad, Long> {
-} 
